6b5e64910d250b327ec3ea0786b0844bebcf9fc8
Sohail Siadatnejad <sohale@gmail.com>
2018-10-23 12:19:31 +0100

    tests use 'fetch'. Much better.

--- tests/api-test.js
+++ tests/api-test.js
@@ -16,7 +16,13 @@ https://github.com/vesln/hippie
 */
 
 var hippie = require('hippie');
+
+var fetch = require('node-fetch');
+// import fetch from 'node-fetch';
+
 console.log("api-test.js .");
+
+
 
 const API_BASE = 'http://localhost:5000'
 const API = '/progimage.com/api/v1.0/'
@@ -57,7 +63,7 @@ function with_file_contents(file_name, content_callback) {
     });
 }
 
-function test_upload(file_name) {
+function test_upload____failed_attempt(file_name) {
     with_file_contents(file_name,function(content){
         console.log("uploading content:", content);
 
@@ -73,21 +79,60 @@ function test_upload(file_name) {
             console.log("caught");
         });
 
+        // .auth('user', 'password')
+
         hippie()
-        //.json()
+        .json()
         .base(API_BASE)
+        .send(content)
+        /*.send({ files: {
+            'binary_content': content,  // 'binary'
+            'filename': file_name,  // 'original-filename'
+        } })*/
         .post(API+'upload')
         .expectStatus(201)
         .end()
-        /*.catch(function(){
-            console.log("caught");
-        })*/
+        .catch(function(ex) {
+            console.log("Exception caught: "+ex);
+        })
 
 
     })
 }
 
+function test_upload(file_name) {
+    with_file_contents(file_name,function(content){
+        console.log("uploading content:", content);
+        //var f = fetch(API_BASE+API+'upload');
+        var body = {content:content};
+        //console.log(f);
+        fetch(API_BASE+API+'upload', {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: { 'Content-Type': 'application/json' },
+            })
+        .then(
+            (response) => {
+                //return response.json();
+                console.log("OK! response:");
+                console.log(response.json());
+            },
+            (rejection) => {
+                console.log("REST sent ERROR:", err);
+            }
+        )
+        .catch(
+             (err) =>
+            console.log("REST sent ERROR:", err)
+        );
+    })
+}
+
 test_upload('./images/tiny_butterfly.jpg');
+
+
+
+//fetch('http://jsonplaceholder.typicode.com/posts/1')
 
 /*
 const fs = require('fs');
