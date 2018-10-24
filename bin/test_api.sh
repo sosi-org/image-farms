de658adad1000b1583d70fbc222f7b7268196d91
Sohail Siadatnejad <sohale@gmail.com>
2018-10-24 15:19:51 +0100

    IMAGESTORE ENV variable used by client to check if the files are created/deleted. (By physically checking the file system)

--- bin/test_api.sh
+++ bin/test_api.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cd tests
-# IMAGESTORE="../imagestore"
+# IMAGESTORE ENV variable used by client to check if the files are created/deleted. (By physically checking the file system)
+export IMAGESTORE="../imagestore"
 node ./api-test.js
 cd ..
